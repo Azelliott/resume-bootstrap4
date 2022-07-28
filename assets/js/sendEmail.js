75bbdeab6203ab0928d730f7128c67366e423b06
Azelliott <jukic.alen@gmail.com>
2022-07-28 11:07:45 +0100

    Finish implementing email integration in contacts.html

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,9 @@
 function sendMail(contactForm) {
     emailjs.init("nS4nxz2OFMKUQZ5e9");
     var templateParams = {
-        'from_name': contactForm.name.value,
-        'from_email': contactForm.emailaddress.value,
-        'project_request': contactForm.projectsummary.value,
+        from_name: contactForm.name.value,
+        from_email: contactForm.emailaddress.value,
+        project_request: contactForm.projectsummary.value,
     };
 
     emailjs.send('service_9htuvvn', 'template_e0opvkn', templateParams)
